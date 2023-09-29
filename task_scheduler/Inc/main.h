5fe55d1d55e87d98b97159c0de9e1648ff1184a7
mei30 <meisamjafarimosleh@gmail.com>
2023-09-29 10:36:52 +0330

    Complete task scheduler

--- task_scheduler/Inc/main.h
+++ task_scheduler/Inc/main.h
@@ -1,35 +1,37 @@
-/*
- * main.h
- *
- *  Created on: Sep 16, 2023
- *      Author: meisam
- */
-
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define DUMMY_XPSR 0x00100000
 
-#define MAX_TASKS 4
+#define MAX_TASKS   5
 
-#define SIZE_TASK_STACK 1024U
-#define SIZE_SCHEDULER_STACK 1024U
+/* some stack memory calculations */
+#define SIZE_TASK_STACK          1024U
+#define SIZE_SCHED_STACK         1024U
 
-#define SRAM_START  0x20000000U
-#define SRAM_SIZE (128 * 1024)
-#define SRAM_END  (SRAM_START + SRAM_START)
+#define SRAM_START               0x20000000U
+#define SIZE_SRAM                ( (128) * (1024))
+#define SRAM_END                 ((SRAM_START) + (SIZE_SRAM) )
 
-#define TASK1_STACK_START SRAM_END
-#define TASK2_STACK_START (SRAM_END - (1 * SIZE_TASK_STACK))
-#define TASK3_STACK_START (SRAM_END - (2 * SIZE_TASK_STACK))
-#define TASK4_STACK_START (SRAM_END - (3 * SIZE_TASK_STACK))
-
-#define SCHEDULER_STACK_START (SRAM_END - (4 * SIZE_TASK_STACK))
+#define T1_STACK_START           SRAM_END
+#define T2_STACK_START           ( (SRAM_END) - (1 * SIZE_TASK_STACK) )
+#define T3_STACK_START           ( (SRAM_END) - (2 * SIZE_TASK_STACK) )
+#define T4_STACK_START           ( (SRAM_END) - (3 * SIZE_TASK_STACK) )
+#define IDLE_STACK_START         ( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START        ( (SRAM_END) - (5 * SIZE_TASK_STACK) )
 
 #define TICK_HZ 1000U
 
 #define HSI_CLOCK         		16000000U
 #define SYSTICK_TIM_CLK   		HSI_CLOCK
 
+
+#define DUMMY_XPSR  0x01000000U
+
+#define TASK_READY_STATE  0x00
+#define TASK_BLOCKED_STATE  0XFF
+
+#define INTERRUPT_DISABLE()  do{__asm volatile ("MOV R0,#0x1"); asm volatile("MSR PRIMASK,R0"); } while(0)
+
+#define INTERRUPT_ENABLE()  do{__asm volatile ("MOV R0,#0x0"); asm volatile("MSR PRIMASK,R0"); } while(0)
 
 #endif /* MAIN_H_ */
